e770c6f4a6a06837ceefc6f34d8ffc45a30bd9d0
ticonetster <19826920+ticonetster@users.noreply.github.com>
2022-07-22 22:16:42 -0700

    add dialect

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    process.env.NODE_ENV
+    {
+      dialect: 'mysql'
+    }
   );
 }
 
